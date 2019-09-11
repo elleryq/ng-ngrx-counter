7c5da0bca16ddd46aae866cd6e732eb5d70e64b4
Yan-ren Tsai <ellery@solidyear.com.tw>
2019-09-11 13:19:01 +0800

    Try ngrx counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-ngrx-counter';
+  counter: Observable<number>;
+
+  constructor(private store: Store<number>){
+    this.counter = store.select('counter');
+  }
+  increment() {
+    this.store.dispatch({type: 'INCREMENT'});
+  }
+
+  decrement() {
+    this.store.dispatch({type: 'DECREMENT'});
+  }
+
+  reset() {
+    this.store.dispatch({type: 'RESET'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { counterReducer } from './app.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({counter: counterReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
